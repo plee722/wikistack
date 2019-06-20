a85df6eeeb46c2b1f697b26355a7e1a1852b2043
Peter Lee <pil206@nyu.edu>
2019-06-20 12:01:35 -0400

    wikistack commit 3

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
+const layout = require("./views/layout");
 
+app.use(morgan);
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send('Hello World');
+  res.send(layout(""));
 });
 
 
-const PORT = 1337;
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
